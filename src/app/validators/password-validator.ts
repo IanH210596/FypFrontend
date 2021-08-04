cf26c2b3bc2e0b805512c520839399d1025558d0
IanH120698 <77413329+IanH120698@users.noreply.github.com>
2021-08-05 00:04:07 +0100

    Commit 05Aug2021

--- src/app/validators/password-validator.ts
+++ src/app/validators/password-validator.ts
@@ -1,23 +1,5 @@
-import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-
-// export function matchingControlValidator(controlName: AbstractControl, matchingControlName: AbstractControl) {
-//   return (formGroup: FormGroup) => {
-
-//       const control = formGroup.controls[controlName];
-//       const matchingControl = formGroup.controls[matchingControlName];
-
-//       if (matchingControl.errors && !matchingControl.errors.mustMatch) {
-//           return;
-//       }
-
-//       if (control.value !== matchingControl.value) {
-//           matchingControl.setErrors({ mustMatch: true });
-//       } else {
-//           matchingControl.setErrors(null);
-//       }
-//   }
-// }
 
 export const passwordValidator: ValidatorFn = (form: FormGroup): ValidationErrors | null => {
   if (form.get('password').value === form.get('confirmPassword').value) {
